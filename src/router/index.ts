e43a8f976860ee05de3857d42b27aeb61fa9c669
Aufa Nirsad <78183137+fanrsd@users.noreply.github.com>
2025-04-30 09:24:33 +0700

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,7 @@ export { default as ff } from './ff'
 export { default as gi } from './gi'
 export { default as hi } from './hi'
 export { default as hsr } from './hsr'
+export { default as hok } from './hok'
 export { default as la } from './la'
 export { default as lad } from './lad'
 export { default as mcgg } from './mcgg'
@@ -17,20 +18,3 @@ export { default as sm } from './sm'
 export { default as sus } from './sus'
 export { default as valo } from './valo'
 export { default as zzz } from './zzz'
-
-export async function hok(id: number): Promise<Result> {
-  try {
-    const response = await fetch(`https://gopay.co.id/games/v1/order/prepare/HOK?userId=${id}&zoneId=`)
-    const data = await response.json()
-    return {
-      success: true,
-      message: data.message,
-      name: data.data
-    }
-  } catch (error) {
-    return {
-      success: false,
-      message: 'Not found'
-    }
-  }
-}
