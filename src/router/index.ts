76f1a101ffe16f1acb7ce1f98b8ea9a12c28096b
Aufa Nirsad <78183137+fanrsd@users.noreply.github.com>
2025-04-30 09:08:37 +0700

    Update index.ts
    add hok

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,5 @@
+import { Result } from '../utils'
+
 export { default as ag } from './ag'
 export { default as aov } from './aov'
 export { default as cod } from './cod'
@@ -15,3 +17,20 @@ export { default as sm } from './sm'
 export { default as sus } from './sus'
 export { default as valo } from './valo'
 export { default as zzz } from './zzz'
+
+export async function hok(id: number): Promise<Result> {
+  try {
+    const response = await fetch(`https://gopay.co.id/games/v1/order/prepare/HOK?userId=${id}&zoneId=`)
+    const data = await response.json()
+    return {
+      success: true,
+      message: data.message,
+      name: data.data
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: 'Not found'
+    }
+  }
+}
