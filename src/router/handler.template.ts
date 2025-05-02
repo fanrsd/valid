44a52609a1c86ae763eabba1449e620d442d63c9
Aufa Nirsad <78183137+fanrsd@users.noreply.github.com>
2025-05-03 05:10:09 +0700

    Update handler.template.ts

--- src/router/handler.template.ts
+++ src/router/handler.template.ts
@@ -1,8 +1,19 @@
-import { hitCoda, Result } from '../utils'
+import { hitCoda, hitGopay, Result } from '../utils'
 
 export default async function gameName(id: string): Promise<Result> {
   const body = `` 
   const data = await hitCoda(body)
+  return {
+    success: true,
+    game: '',
+    id,
+    name: data.confirmationFields.username
+  }
+}
+
+export default async function gameNames(id: string): Promise<Result> {
+  const body = `` 
+  const data = await hitGopay(body)
   return {
     success: true,
     game: '',
