f290fd9ec642b2641122bfca29b8eceee9d4aafb
Aufa Nirsad <78183137+fanrsd@users.noreply.github.com>
2025-05-03 05:41:07 +0700

    Update handler.ts

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,6 @@
 import { allowedMethod, timeNow } from './utils'
-import serveResult from './helpers'
-import serveGopayResult from './helpers'
+import  from './helpers'
+import { serveResult, serveGopayResult } from './helpers'
 
 export default async function checkCache(request: Request): Promise<Response> {
   const now = timeNow()
